67454ff9527167feb499106c615c42079f8fc3b1
voetberg <magpie127@gmail.com>
2023-10-05 15:58:46 -0500

    testing failed but existant

--- nugraph/explain_graph/explain_local.py
+++ nugraph/explain_graph/explain_local.py
@@ -1,6 +1,101 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+from typing import Any
+import tqdm 
+import pandas as pd 
+import os 
+
+import nugraph as ng
+import lightning.pytorch as pl 
+
 
 class ExplainLocal(ABC): 
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(self, 
+                 data_path:str, 
+                 out_path:str = "explainations/",
+                 checkpoint_path:str=None, 
+                 batch_size:int=16) -> None:
+        """
+        Abstract class 
+        Perform a local explaination method on a single datapoint
+
+        Args:
+            data_path (str): _description_
+            out_path (str, optional): _description_. Defaults to "explainations/".
+            checkpoint_path (str, optional): _description_. Defaults to None.
+            batch_size (int, optional): _description_. Defaults to 16.
+        """
+        self.model = self.load_checkpoint(checkpoint_path) if checkpoint_path is not None else NuGraph2()
+        self.data = self.load_data(data_path, batch_size)
+        self.explainations = [] 
+        self.out_path = out_path
+
+    def load_checkpoint(self, checkpoint_path:str):
+        """Load a saved checkpoint to perform inference
+
+        Returns:
+            _type_: _description_
+        """
+        model = ng.models.NuGraph.load_from_checkpoint(checkpoint_path) 
+        model.eval() 
+        return model 
+
+    def inference(self): 
+        """_summary_
+        """
+        accelerator, devices = ng.util.configure_device()
+        trainer = pl.Trainer(accelerator=accelerator, devices=devices,
+                         logger=False)
+        predictions = trainer.predict(self.model, dataloaders=self.data.test_dataloader())
+        for _, batch in enumerate(tqdm.tqdm(predictions)):
+            for data in batch.to_data_list():
+                self.explainations.append(self.explain(self.model, data)) 
         
+        self.explainations = pd.concat(self.explainations)
+
+
+    def load_data(self, data_path, batch_size): 
+        """_summary_
+
+        Args:
+            data_path (_type_): _description_
+            batch_size (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        return ng.data.H5DataModule(data_path, batch_size=batch_size)
+
+    @abstractmethod
+    def explain(self, *args, **kwds): 
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
+        return "" 
+    
+    @abstractmethod
+    def visualize(self, *args, **kwrds): 
+        """_summary_
+        """
+        pass 
+    
+    def save(self, format:str='hdf'): 
+        """_summary_
+
+        Args:
+            format (str, optional): _description_. Defaults to 'hdf'.
+        """
+        assert format in ['hdf', 'csv'], "format must be 'hdf' or 'csv'"
+        if not os.path.exists(self.out_path): 
+            os.makedirs(self.out_path)
+
+        save_file = f"{self.out_path}{}"
+        {
+            "hdf":lambda x:x.to_hdf(save_file, format='table'), 
+            'csv': lambda x: x.to_csv(save_file)
+        }[format](self.explainations)
+
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        self.inference()
+        self.save() 
--- tests/test_local_explain.py
+++ tests/test_local_explain.py
@@ -1,36 +1,38 @@
 import pytest
-from nugraph.explain_graph import ExplainLocal
-import pytorch 
+import torch 
 import os 
 
+from nugraph.explain_graph import ExplainLocal
+from nugraph.data import H5DataModule
 
-def test_abstract(): 
-    with pytest.raises(NotImplementedError): 
-        ExplainLocal()
+checkpoint_path = "paper.ckpt" # Not for remote ci/cd
+data_path = ""
 
-def test_load_model(): 
-    class Child(ExplainLocal): 
-        def __init__(self) -> None:
-            super().__init__()
+@pytest.fixture
+def Child(): 
+    class thing(ExplainLocal): 
+        def __init__(self, data_path: str,checkpoint_path: str = None, out_path: str = "explainations/",  batch_size: int = 16) -> None:
+            super().__init__(data_path, out_path, checkpoint_path, batch_size)
+    return thing 
 
-    model = Child().model 
-    assert isinstance(model, pytorch.nn.Module)
+def test_load_model(Child): 
+    model = Child(data_path=data_path, checkpoint_path=checkpoint_path).model 
+    assert isinstance(model, torch.nn.Module)
 
-def test_load_data(): 
-    class Child(ExplainLocal): 
-        def __init__(self) -> None:
-            super().__init__()
+def test_load_data(Child): 
+    data = Child(data_path=data_path, checkpoint_path=checkpoint_path).data 
+    assert isinstance(data, H5DataModule)
 
-    data = Child().data 
-    #TODO - Data format? 
-    assert isinstance(data, pyg)
+def test_no_model(Child): 
+    model = Child(data_path=data_path).model 
+    assert isinstance(model, torch.nn.Module)
 
 def test_visualize_subclass(): 
     class Child(ExplainLocal): 
-        def __init__(self) -> None:
-            super().__init__()
-        
-        def plot(self, path): 
+        def __init__(self, checkpoint_path: str = None, out_path: str = "explainations/",  batch_size: int = 16) -> None:
+            super().__init__(data_path, out_path, checkpoint_path, batch_size)
+
+        def visualize(self, path): 
             open(path, "w")
     
     path = "test_file.png"
@@ -43,5 +45,6 @@ def test_visualize_subclass():
 
 
 def test_save_results_subclass(): 
-    #TODO basic 
+    #TODO basic save structure for the cluster. I don't know what it should be 
+    # Yet.
     pass 
