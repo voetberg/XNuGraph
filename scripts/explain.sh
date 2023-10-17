327e255a3738be52c2bea4de53f86797af272bb9
voetberg <maggiev@fnal.gov>
2023-10-17 09:46:10 -0500

    working assuming they are being run from a srun command

--- scripts/explain.sh
+++ scripts/explain.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+set -e 
+
 checkpoint=/wclustre/fwk/exatrkx/data/uboone/CHEP2023/paper.ckpt 
 algorithm=GNNExplainer
 outfile=/wclustre/fwk/exatrkx/data/uboone/CHEP2023/test.gnn.h5
@@ -5,4 +8,9 @@ data_path=/wclustre/fwk/exatrkx/data/uboone/CHEP2023/CHEP2023-small.evt.h5
 batch_size=16
 test=True
 
-srun --unbuffered --pty -A fwk --partition=gpu_gce --gres=gpu:v100:1 --qos=test --nodes=1 --time=01:00:00 --ntasks-per-node=4 python3 scripts/explain.py --checkpoint $checkpoint  --algorithm $algorithm  --outfile $outfile --data_path $data_path --batch_size $batch_size --test $test
+conda_env=/work1/fwk/maggiev/miniforge3/bin
+env_name=nugraph
+
+source $conda_env/activate 
+conda activate $env_name
+python3 /work1/fwk/maggiev/NuGraph/scripts/explain.py --checkpoint $checkpoint  --algorithm $algorithm  --outfile $outfile --data_path $data_path --batch_size $batch_size --test $test
