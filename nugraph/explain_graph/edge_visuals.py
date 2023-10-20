5fc701b77d7b259ca5d493715cca87061b0c71f8
voetberg <maggiev@fnal.gov>
2023-10-20 10:54:43 -0500

    working on graphs in networkx

--- nugraph/explain_graph/edge_visuals.py
+++ nugraph/explain_graph/edge_visuals.py
@@ -3,34 +3,85 @@ import networkx as nx
 
 import torch_geometric as pyg
 import torch
+import pandas as pd
 from nugraph.explain_graph.load import Load
 
 class EdgeVisuals: 
 
-    def __init__(self) -> None:
-        load = Load()
+    def __init__(self, test=False) -> None:
+        load = Load(test=test)
         self.model = load.model
         self.data = load.data.dataset
-
-    def weight_edges(self): 
-        pass 
+        self.predictions = load.predictions
 
     def plot_truth_labels(self): 
         pass 
 
+    def plot_model_labels(self): 
+        pass
+
     def plot_plane(self, graph, plane='u'): 
-        subgraph = graph.node_type_subgraph(node_types=[plane])
-        nx.draw(subgraph, pos=subgraph.pos)
+        
+        subgraph_nx = nx.Graph()
+        # position = graph[plane]['pos'] 
+        # x = graph[plane]['x'] 
 
-    def plot(self, data_index=0): 
+        # for pos, x_val in zip(position, x):
+        #     subgraph_nx.add_node(pos, x=x_val)
+
+        edge_node_1 = graph[(plane, "plane", plane)]['edge_index'][0]
+        edge_node_2 = graph[(plane, "plane", plane)]['edge_index'][1]
+        for v, u in zip(edge_node_1, edge_node_2):
+            subgraph_nx.add_edge(v, u)
+
+        position = {node: graph[plane]['pos'][node] for node in range(len(graph[plane]['pos']))}
+
+        true_labels = {node: graph[plane]['sem_label'] for node in range(len(graph[plane]['pos']))}
+        pred_labels = {node: graph[plane]['pred_label'] for node in range(len(graph[plane]['pos']))}
+
+        truth = nx.draw(subgraph_nx, pos=position, labels=true_labels) 
+        prediction = nx.draw(subgraph_nx, pos=position, labels=pred_labels) 
+        plt.savefig("test_plot.png")
+        return truth, prediction
+
+    def get_graph(self, data_index): 
+
         graph = self.data.get(data_index)
+        prediction = self.predictions[data_index]
 
-        del graph['metadata']
-        del graph['evt']
+        for plane in ['u', 'v', 'y']: 
+            plane_prediction = pd.Categorical(prediction['x_semantic'][plane]).codes
+            graph[plane]['pred_label'] = plane_prediction
+            graph[plane]['sem_label'] = pd.Categorical(prediction['y_semantic'][plane]).codes
 
-        nx_graph = pyg.utils.to_networkx(graph)
-        self.plot_plane(nx_graph, plane='u')
+        return graph 
+    
+    def plot(self, data_index=0): 
+
+        graph = self.get_graph(data_index)
+
+        fig, subplots = plt.subplots(2, 3)
+        
+        u_plane = self.plot_plane(graph, plane='u')
+        v_plane = self.plot_plane(graph, plane='v')
+        y_plane = self.plot_plane(graph, plane='y')
+
+        subplots[0,1], subplots[0,2] = u_plane[0], u_plane[1]
+        subplots[0,2].set_x_label("U Plane")
+        subplots[0,1].set_y_label("Prediction")
+        subplots[0,2].set_y_label("Truth")
+
+        subplots[1,1], subplots[1,2] = v_plane[0], v_plane[1]
+        subplots[1,2].set_x_label("V Plane")
+
+        subplots[2,1], subplots[2,2] = y_plane[0], y_plane[1]
+        subplots[2,2].set_x_label("y Plane")
+
+        fig.supxlabel("wire")
+        fig.supylabel("time")
+
+        plt.savefig("test_plot.png")
 
 
 if __name__=='__main__': 
-    EdgeVisuals().plot()
+    EdgeVisuals(test=True).plot()
--- nugraph/explain_graph/load.py
+++ nugraph/explain_graph/load.py
@@ -1,6 +1,9 @@
 import torch
+import pytorch_lightning as pl
+
 from nugraph.models import NuGraph2
 from nugraph.data import H5DataModule
+from nugraph import util
 
 
 
@@ -8,9 +11,14 @@ class Load:
     def __init__(self,
                  checkpoint_path="/wclustre/fwk/exatrkx/data/uboone/CHEP2023/paper.ckpt", 
                  data_path="/wclustre/fwk/exatrkx/data/uboone/CHEP2023/CHEP2023.gnn.h5", 
-                 batch_size=1) -> None:
+                 batch_size=1, 
+                 test=False) -> None:
         self.model = self.load_checkpoint(checkpoint_path) if checkpoint_path is not None else NuGraph2()
         self.data = self.load_data(data_path, batch_size)
+        if test: 
+            self.data = next(iter(self.data))
+            
+        self.predictions = self.make_predictions()
 
     def load_checkpoint(self, checkpoint_path, graph=NuGraph2): 
         # Assumed pre-trained model that can perform inference on the loaded data
@@ -33,3 +41,10 @@ class Load:
 
     def load_data(self, data_path, batch_size=1): 
         return H5DataModule(data_path, batch_size=batch_size).val_dataloader()
+    
+    def make_predictions(self): 
+        accelerator, device = util.configure_device()
+        trainer = pl.Trainer(accelerator=accelerator,
+                            device=device,
+                            logger=False)
+        predictions = trainer.predict(self.model, dataloaders=self.data)
