3bf272ce162aada5e41839c385a94cef023f08cf
voetberg <magpie127@gmail.com>
2023-11-28 09:51:22 -0600

    outline for abstract class and matching tests

--- nugraph/explain_graph/explain_network.py
+++ nugraph/explain_graph/explain_network.py
@@ -6,7 +6,14 @@ class ExplainNetwork:
         Abstract method for iterating a network and measuring a level of explaination for each iteration
         "Iteration" is a general term, can refer to message passing or stepping through layers. 
     """
-    def __init__(self, checkpoint_path, data_path, batch_size, test, planes, semantic_classes) -> None:
+    def __init__(self, 
+                 checkpoint_path, 
+                 data_path, 
+                 batch_size, 
+                 test, 
+                 planes=['u','v','y'], 
+                 semantic_classes = ['MIP','HIP','shower','michel','diffuse'],
+) -> None:
         load = Load(checkpoint_path=checkpoint_path, data_path=data_path, batch_size=batch_size, test=test, planes=planes)
         self.planes = planes
         self.data = load.data
@@ -44,17 +51,34 @@ class ExplainNetwork:
         def make_ones(in_shape): 
             # Does the same thing as the linear layer, but there is no grad and it's all ones 
             return {plane:self.linear_decoder(in_shape) for plane in self.planes}
+        
+        input_inshape = 1
+        encoder_inshape = self.model.encoder.net[self.planes[0]][0].weight.shape[0]
+        planar_inshape = 1
+        nexus_inshape = 1
 
         return {
-            "encoder": make_ones(),
-            "planar":make_ones(),
-            "nexus":make_ones(), 
-            "decoder":[make_ones() for _ in self.model.decoders]
+            "input": make_ones(input_inshape),
+            "encoder": make_ones(encoder_inshape),
+            "planar":make_ones(planar_inshape),
+            "nexus":make_ones(nexus_inshape), 
         }
 
     def step_network(self): 
         """
             Iterate over the graph and perforce inference at each step using the static decoders
+        """
+
+        input_decoded = None 
+        encoder_decoded = None 
+        planar_decoded = None 
+        nexus_decoded = None 
+        output = None
+        
+        return input_decoded, encoder_decoded, planar_decoded, nexus_decoded, output
+
+    def forward(self): 
+        """_summary_
         """
         raise NotImplemented
 
--- tests/test_network_explain.py
+++ tests/test_network_explain.py
@@ -1,2 +1,25 @@
 import pytest 
+from nugraph.explain_graph.explain_network import ExplainMessages, ExplainNetwork
 
+def test_init_abstract_defaults(): 
+    explain = ExplainNetwork()
+    assert hasattr(explain.data)
+    assert hasattr(explain.model)
+
+    assert explain.semantic_classes == ['MIP','HIP','shower','michel','diffuse']
+    assert explain.planes == ['u', 'v', 'y']
+
+def test_make_linear_decoder():
+    e = ExplainNetwork()
+    encoder = e.model.encoder.net
+    decoder = e.linear_decoder(in_shape=encoder[e.planes[0]][0].weight.shape[0])
+    
+    out = decoder.forward(encoder.forward(e.data)) 
+    assert out is not None 
+
+def test_all_static_decoders(): 
+    e = ExplainNetwork()
+    decoder_out = e.step_network()
+    
+    assert None not in decoder_out
+    
