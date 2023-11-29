395bd7136f3e47664edafc13ffe731c7e05b5258
voetberg <magpie127@gmail.com>
2023-11-29 07:52:31 -0600

    Trying to match the required size - mocking the decoder format

--- nugraph/explain_graph/explain_network.py
+++ nugraph/explain_graph/explain_network.py
@@ -1,4 +1,4 @@
-from explain_graph.load import Load
+from nugraph.explain_graph.load import Load
 import torch 
 
 class ExplainNetwork:
@@ -7,35 +7,47 @@ class ExplainNetwork:
         "Iteration" is a general term, can refer to message passing or stepping through layers. 
     """
     def __init__(self, 
-                 checkpoint_path, 
-                 data_path, 
-                 batch_size, 
-                 test, 
+                 checkpoint_path=None, 
+                 data_path=None, 
+                 batch_size:int=1, 
+                 test:bool=False, 
                  planes=['u','v','y'], 
                  semantic_classes = ['MIP','HIP','shower','michel','diffuse'],
 ) -> None:
-        load = Load(checkpoint_path=checkpoint_path, data_path=data_path, batch_size=batch_size, test=test, planes=planes)
+        
+        self.load = Load(checkpoint_path=checkpoint_path, data_path=data_path, batch_size=batch_size, test=test, planes=planes)
         self.planes = planes
-        self.data = load.data
-        self.model = load.model
+        self.data = self.load.data
+        self.model = self.load.model
 
         self.semantic_classes = semantic_classes
-
-        self.static_decoder = self.make_static_decoders()
+        self.make_static_decoders()
 
     def linear_decoder(self, in_shape): 
         num_classes = len(self.semantic_classes)
+        planes = self.planes
         class LinearDecoder: 
             def __init__(self, in_shape) -> None:
-                self.decoder = []
-                self.num_classes = num_classes
-                for _ in range(num_classes):
-                    self.decoder.append(torch.ones(in_shape, 1))
+                self.decoder = {}
+                for plane in planes: 
+                    self.decoder[plane] = self.class_decoder(in_shape)
  
-            def forward(self, X): 
-                x = torch.tensor_split(X, self.num_classes, dim=1)
-                return torch.cat([ net(x[i]) for i, net in enumerate(self.decoder) ], dim=1)
+            def class_decoder(self, in_shape): 
+                decoder =  []
+                for _ in range(num_classes):
+                    decoder.append(torch.ones(in_shape, 1))
+                return decoder 
+            
+            def class_decoder_forward(self, X, decoder): 
+                x = torch.tensor_split(X, num_classes, dim=1) 
+                return torch.cat([ net*x[i] for i, net in enumerate(decoder)], dim=1) 
 
+            def forward(self, x): 
+                return {
+                    plane: self.class_decoder_forward(x[plane], self.decoder[plane]).squeeze(dim=-1) 
+                    for plane in planes
+                    } 
+            
         return LinearDecoder(in_shape)
 
     def make_static_decoders(self): 
@@ -50,35 +62,53 @@ class ExplainNetwork:
 
         def make_ones(in_shape): 
             # Does the same thing as the linear layer, but there is no grad and it's all ones 
-            return {plane:self.linear_decoder(in_shape) for plane in self.planes}
+            return {plane: self.linear_decoder(in_shape) for plane in self.planes}
         
-        input_inshape = 1
-        encoder_inshape = self.model.encoder.net[self.planes[0]][0].weight.shape[0]
-        planar_inshape = 1
+        input_inshape = self.model.encoder.net[self.planes[0]][0].net[0].weight.shape[1]
+        input_inshape = int(input_inshape/len(self.planes))
+
+        encoder_inshape = self.model.encoder.net[self.planes[0]][0].net[0].weight.shape[0]
+
+        planar_inshape = self.model.plane_net.net[self.planes[0]].node_net[-2].net[0].weight.shape[0]
+
         nexus_inshape = 1
 
-        return {
-            "input": make_ones(input_inshape),
-            "encoder": make_ones(encoder_inshape),
-            "planar":make_ones(planar_inshape),
-            "nexus":make_ones(nexus_inshape), 
-        }
+        self.input_decoder = make_ones(input_inshape)#self.linear_decoder(input_inshape) 
+        self.encoder_decoder = self.linear_decoder(encoder_inshape)
+        self.planar_decoder = self.linear_decoder(planar_inshape)
+        self.nexus_decoder = self.linear_decoder(nexus_inshape)
+        
 
     def step_network(self): 
         """
             Iterate over the graph and perforce inference at each step using the static decoders
         """
+        x, edge_index_plane, edge_index_nexus, nexus, batch = self.load.unpack(self.data)
+        forward = x.copy()
+        input_decoded =  {plane: self.input_decoder[plane].forward(forward) for plane in self.planes}
 
-        input_decoded = None 
-        encoder_decoded = None 
-        planar_decoded = None 
-        nexus_decoded = None 
-        output = None
+        forward = self.model.encoder.forward(forward)
+        encoder_decoded = self.encoder_decoder.forward(forward)
+
+        # I wish it wouldn't but the plane net just modifies the input data instead of making a copy
+        for p in self.planes:
+            s = x[p].detach().unsqueeze(1).expand(-1, forward[p].size(1), -1)
+            forward[p] = torch.cat((forward[p], s), dim=-1)
         
+        self.model.plane_net(forward, edge_index_plane)
+        planar_decoded = self.planar_decoder.forward(forward) 
+
+        self.model.nexus_net(forward, edge_index_nexus, nexus)
+        nexus_decoded = self.nexus_decoder.forward(forward) 
+
+        output = {}
+        for decoder in self.model.decoders:
+            output.update(decoder(forward, batch))
+
         return input_decoded, encoder_decoded, planar_decoded, nexus_decoded, output
 
     def forward(self): 
-        """_summary_
+        """
         """
         raise NotImplemented
 
@@ -93,8 +123,6 @@ class ExplainMessages(ExplainNetwork):
     def __init__(self, checkpoint_path, data_path, batch_size, test, planes) -> None:
         super().__init__(checkpoint_path, data_path, batch_size, test, planes)
 
-    def step_network(self):
-        return super().step_network()
     
     def calculate_percent_explained(self):
         """Using a entropy measure to see the information gain over the previous step"""
--- tests/test_network_explain.py
+++ tests/test_network_explain.py
@@ -2,24 +2,37 @@ import pytest
 from nugraph.explain_graph.explain_network import ExplainMessages, ExplainNetwork
 
 def test_init_abstract_defaults(): 
-    explain = ExplainNetwork()
-    assert hasattr(explain.data)
-    assert hasattr(explain.model)
+    explain = ExplainNetwork(test=True)
+    assert hasattr(explain, 'data')
+    assert hasattr(explain, 'model')
 
     assert explain.semantic_classes == ['MIP','HIP','shower','michel','diffuse']
     assert explain.planes == ['u', 'v', 'y']
 
 def test_make_linear_decoder():
-    e = ExplainNetwork()
-    encoder = e.model.encoder.net
-    decoder = e.linear_decoder(in_shape=encoder[e.planes[0]][0].weight.shape[0])
-    
-    out = decoder.forward(encoder.forward(e.data)) 
+    e = ExplainNetwork(test=True)
+
+    encoder = e.model.encoder
+    encoder_size = encoder.net[e.planes[0]][0].net[0].weight.shape[0]
+    decoder = e.linear_decoder(in_shape=encoder_size)
+    data = e.load.unpack(e.data)[0]
+    forward = encoder.forward(data)
+    out = decoder.forward(forward)
+
     assert out is not None 
+    assert type(out) == dict 
+    assert list(out.keys()) == e.planes
+    for plane in e.planes: 
+        assert out[plane].shape == data[plane].shape
 
 def test_all_static_decoders(): 
-    e = ExplainNetwork()
+    e = ExplainNetwork(test=True)
     decoder_out = e.step_network()
-    
     assert None not in decoder_out
-    
+
+    for output in decoder_out: 
+        for _, plane in enumerate(e.planes): 
+            expected_size = decoder_out[-1]['x_semantic'][plane].shape
+            print(output)
+            assert output[plane].shape == expected_size
+        
