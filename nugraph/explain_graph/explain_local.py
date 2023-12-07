8e4a02c22d52bf256af49c563a9604dd89304f06
voetberg <magpie127@gmail.com>
2023-12-07 08:34:06 -0600

    Framework class for extracting results from the network to work with the explain.py script

--- nugraph/explain_graph/explain_local.py
+++ nugraph/explain_graph/explain_local.py
@@ -1,6 +1,5 @@
 import tqdm 
 import os 
-from nugraph import data, models
 from nugraph.explain_graph.load import Load
 import h5py
 
@@ -8,9 +7,7 @@ import torch
 from datetime import datetime 
 
 from torch_geometric.explain import Explanation, metric
-from torch_geometric.data import Batch, HeteroData
-from torch_geometric.utils import unbatch
-from functools import partial
+
 
 class ExplainLocal:
     def __init__(self, data_path:str, out_path:str = "explainations/",checkpoint_path:str=None, batch_size:int=16, test:bool=False):
@@ -35,35 +32,7 @@ class ExplainLocal:
         self.explainer = None
 
     def process_graph(self, graph):
-        """
-        Remove double connections from point to point - the graph is bidirectional and should be treated as such
-        """ 
-        # def remove_double_connections(edge): 
-        #     print(edge)
-        #     edge_tuples = [{node1, node2} for node1, node2 in zip(edge[0], edge[1])] 
-        #     edge_tuple_counts = {edge_tuples.index(key) for key in edge_tuples}
-        #     print(edge_tuples)
-        #     print(len(edge[0]))
-        #     print(len(edge_tuple_counts))
-        #     return edge 
-            
-        # for plane in self.model.planes: 
-        #     plane_edges = graph[plane, 'plane', plane]['edge_index']
-        #     nexus_edges = graph[plane, 'nexus', 'sp']['edge_index']
-        #     graph[plane, 'plane', plane]['edge_index'] = remove_double_connections(plane_edges)
-        #     graph[plane, 'nexus', 'sp']['edge_index'] = remove_double_connections(nexus_edges)
-
         return graph 
-
-    def unpack(self, data): 
-        """Unpack the data to be used by the model"""
-        return (
-            data.collect('x'),
-            { p: data[p, 'plane', p].edge_index for p in self.model.planes }, 
-            { p: data[p, 'nexus', 'sp'].edge_index for p in self.model.planes },
-            torch.empty(data['sp'].num_nodes, 0),
-            { p: data[p].batch for p in self.model.planes }
-            )
 
 
     def inference(self, explaintion_kwargs=None): 
--- nugraph/explain_graph/explain_network.py
+++ nugraph/explain_graph/explain_network.py
@@ -1,129 +1,28 @@
 from nugraph.explain_graph.load import Load
+from nugraph.explain_graph.algorithms.linear_probes.probed_network import ""
+
+from nugraph.explain_graph.explain_local import ExplainLocal
 import torch 
 
-class ExplainNetwork:
-    """
-        Abstract method for iterating a network and measuring a level of explaination for each iteration
-        "Iteration" is a general term, can refer to message passing or stepping through layers. 
-    """
+class ExplainNetwork(ExplainLocal):
     def __init__(self, 
-                 checkpoint_path=None, 
-                 data_path=None, 
-                 batch_size:int=1, 
-                 test:bool=False, 
-                 planes=['u','v','y'], 
-                 semantic_classes = ['MIP','HIP','shower','michel','diffuse'],
-) -> None:
-        
-        self.load = Load(checkpoint_path=checkpoint_path, data_path=data_path, batch_size=batch_size, test=test, planes=planes)
-        self.planes = planes
-        self.data = self.load.data
-        self.model = self.load.model
+                 data_path: str, 
+                 out_path: str = "explainations/", 
+                 checkpoint_path: str = None, 
+                 batch_size: int = 16, 
+                 test: bool = False):
+        super().__init__(data_path, out_path, checkpoint_path, batch_size, test)
+        self.explainer = ""
 
-        self.semantic_classes = semantic_classes
-        self.make_static_decoders()
 
-    def linear_decoder(self, in_shape): 
-        num_classes = len(self.semantic_classes)
-        planes = self.planes
-        class LinearDecoder: 
-            def __init__(self, in_shape) -> None:
-                self.decoder = {}
-                for plane in planes: 
-                    self.decoder[plane] = self.class_decoder(in_shape)
- 
-            def class_decoder(self, in_shape): 
-                decoder =  []
-                for _ in range(num_classes):
-                    decoder.append(torch.ones(in_shape, 1))
-                return decoder 
-            
-            def class_decoder_forward(self, X, decoder): 
-                x = torch.tensor_split(X, num_classes, dim=1) 
-                return torch.cat([ net*x[i] for i, net in enumerate(decoder)], dim=1) 
-
-            def forward(self, x): 
-                return {
-                    plane: self.class_decoder_forward(x[plane], self.decoder[plane]).squeeze(dim=-1) 
-                    for plane in planes
-                    } 
-            
-        return LinearDecoder(in_shape)
-
-    def make_static_decoders(self): 
+    def explain(self, data, **kwargs): 
         """
-            Makes a decoder for each part of the network to turn the encoded space into something readable
-        """
-        # Make a dictionary of decoders for the network - one for each part of the network (not including the decoders)
-        # Assuming you can use Nugraph2 as a format guide
-
-        # Static decoder acts as a step between the network and what the next step would be
-        # - in is the same shape as the layer, but out is always the number of classes (acts as the semantic decoder)
-
-        def make_ones(in_shape): 
-            # Does the same thing as the linear layer, but there is no grad and it's all ones 
-            return {plane: self.linear_decoder(in_shape) for plane in self.planes}
-        
-        input_inshape = self.model.encoder.net[self.planes[0]][0].net[0].weight.shape[1]
-        input_inshape = int(input_inshape/len(self.planes))
-
-        encoder_inshape = self.model.encoder.net[self.planes[0]][0].net[0].weight.shape[0]
-
-        planar_inshape = self.model.plane_net.net[self.planes[0]].node_net[-2].net[0].weight.shape[0]
-
-        nexus_inshape = 1
-
-        self.input_decoder = make_ones(input_inshape)#self.linear_decoder(input_inshape) 
-        self.encoder_decoder = self.linear_decoder(encoder_inshape)
-        self.planar_decoder = self.linear_decoder(planar_inshape)
-        self.nexus_decoder = self.linear_decoder(nexus_inshape)
-        
-
-    def step_network(self): 
-        """
-            Iterate over the graph and perforce inference at each step using the static decoders
-        """
-        x, edge_index_plane, edge_index_nexus, nexus, batch = self.load.unpack(self.data)
-        forward = x.copy()
-        input_decoded =  {plane: self.input_decoder[plane].forward(forward) for plane in self.planes}
-
-        forward = self.model.encoder.forward(forward)
-        encoder_decoded = self.encoder_decoder.forward(forward)
-
-        # I wish it wouldn't but the plane net just modifies the input data instead of making a copy
-        for p in self.planes:
-            s = x[p].detach().unsqueeze(1).expand(-1, forward[p].size(1), -1)
-            forward[p] = torch.cat((forward[p], s), dim=-1)
-        
-        self.model.plane_net(forward, edge_index_plane)
-        planar_decoded = self.planar_decoder.forward(forward) 
-
-        self.model.nexus_net(forward, edge_index_nexus, nexus)
-        nexus_decoded = self.nexus_decoder.forward(forward) 
-
-        output = {}
-        for decoder in self.model.decoders:
-            output.update(decoder(forward, batch))
-
-        return input_decoded, encoder_decoded, planar_decoded, nexus_decoded, output
-
-    def forward(self): 
-        """
-        """
-        raise NotImplemented
-
-    def calculate_percent_explained(self): 
-        """
-            Calculate how much each round of message passing through the network is impacting the final result 
+        Impliment the explaination method
         """
         raise NotImplemented
     
-
-class ExplainMessages(ExplainNetwork): 
-    def __init__(self, checkpoint_path, data_path, batch_size, test, planes) -> None:
-        super().__init__(checkpoint_path, data_path, batch_size, test, planes)
-
-    
-    def calculate_percent_explained(self):
-        """Using a entropy measure to see the information gain over the previous step"""
-        return super().calculate_percent_explained()
+    def visualize(self, *args, **kwrds): 
+        """ 
+        Produce a visualization of the explaination
+        """
+        raise NotImplemented 
